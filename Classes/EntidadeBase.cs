4d128de1ba0792283d76e9c7b96913e12690db21
gsennas <gsennaeua@gmail.com>
2021-12-21 11:58:44 -0300

    First Commit

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroDeSeries
+{
+    public abstract class EntidadeBase
+    {
+        public int ID { get; protected set; }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Projeto da DIO - > cadastro de Séries
